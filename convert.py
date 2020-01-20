8563dbd444347ff8384591bca76d47ad1874ca55
BCDeshiG <bcdg55@gmail.com>
2020-01-20 22:46:52 +0000

    Parse splashes.json
    Convert bedrock splashes to text file, also beginning of zip/mcpack support.

--- convert.py
+++ convert.py
@@ -1,41 +1,53 @@
 import sys
 import json
 import os
+import zipfile
 
 def parseArgs():
 	if len(sys.argv) == 1: # No arguments supplied
-		menu()
+		arg1 = input("Enter location of bedrock texture pack: ")
+		arg2 = input("Enter location of destination folder: ")
+		startConversion(arg1, arg2)
 	else:
 		if sys.argv[1] == "-h" or sys.argv[1] == "--help":
 			print("\nUsage: " + sys.argv[0] + " [-h] PACK DEST")
 			print("Convert MC Bedrock pack to Java")
 			print("\nArguments:")
-			print("PACK		Location of Texture folder/mcpack file") # FIXME Implement mcpack support
+			print("PACK		Location of Texture folder/zip/mcpack file")
 			print("DEST		Destination folder of converted pack")
 			print("-h, --help	Displays this message (ofc)")
 			print("\nExample: " + sys.argv[0] + " foo.mcpack 'C:/oofPack'")
 		elif len(sys.argv) != 3:
 			print("YOU TWONK")
 		else:
-			#startConversion(sys.argv[1], sys.argv[2])
-			parseManifest(sys.argv[1], sys.argv[2])
-
-def menu():
-	arg1 = input("Enter location of bedrock texture pack: ")
-	arg2 = input("Enter location of destination folder: ")
-	#startConversion(sys.argv[1], sys.argv[2])
-	parseManifest(arg1, arg2)
+			startConversion(sys.argv[1], sys.argv[2])
 
 def startConversion(arg1, arg2):
-	# Placeholder to confirm args
-	print("arg1: " + arg1)
-	print("arg2: " + arg2)
+	arg1 = parseZip(arg1)
+	parseManifest(arg1, arg2)
+	#parseSplashes(arg1, arg2)
+
+def parseZip(arg1):
+	if arg1.lower().endswith(".zip"):
+		fileExt = ".zip"
+	elif arg1.lower().endswith(".mcpack"):
+		fileExt = ".mcpack"
+	else:
+		return arg1
+	arg1 = os.path.splitext(arg1)[0]
+	with zipfile.ZipFile(arg1 + fileExt,"r") as pack:
+		pack.extractall(arg1) # FIXME handle root folder
+	return arg1
 
 def parseManifest(arg1, arg2):
-	with open(arg1 + "/manifest.json", "r") as file:
-		manifest = json.load(file)
-		head = manifest["header"]
-		desc = head["description"]
+	try:
+		with open(arg1 + "/manifest.json", "r") as file:
+			manifest = json.load(file)
+			head = manifest["header"]
+			desc = head["description"]
+	except:
+		print("Pack does not exist or is invalid")
+		exit()
 	outText = '{"pack": {"description": "'
 	outText += desc + '",'
 	outText += '"pack_format": 5}}'
@@ -45,11 +57,17 @@ def parseManifest(arg1, arg2):
 		file.write(outText)
 		file.close()
 	except:
-		confirm = input("Existing Pack detected, are you sure you want to overwrite it? (Y/N): ")
-		if confirm == "Y" or confirm == "y":
-			file = open(arg2 + "/pack.mcmeta", "w")
-			file.write(outText)
-			file.close()
+		print("Folder already exists or was unable to be created")
+		exit()
+
+def parseSplashes(arg1, arg2):
+	with open(arg1 + "/splashes.json", "r") as file:
+		splashes = json.load(file)
+	outFile = open(arg2 + "/assets/minecraft/texts/splashes.txt", "w") # FIXME Make directories 1st 
+	arr = splashes["splashes"]
+	for i in range(len(arr)):
+		outFile.write(arr[i]+"\n")
+	outFile.close()
 
 
 parseArgs()
